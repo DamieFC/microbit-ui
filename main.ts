621002174100f0803c4f73f18e2735d513fe29d8
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-15 14:18:45 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,6 +24,8 @@ serial.redirectToUSB()
 serial.writeLine("Do you SEE this???")
 basic.pause(2000)
 serial.writeLine("Micro:bit booted!")
+basic.pause(2000)
+serial.writeLine("" + control.deviceSerialNumber() + " is the micro:bit serial number")
 basic.forever(function () {
 	
 })
